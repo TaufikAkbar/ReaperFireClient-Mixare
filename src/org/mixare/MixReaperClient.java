4f25e773bb7758a68876c96f76598ac36b30d87b
Jeryl Cook <info@vanity-soft.com>
2010-11-22 04:35:59 -0500

    first commit

---
+++ src/org/mixare/MixReaperClient.java
@@ -0,0 +1,365 @@
+package org.mixare;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.params.ConnPerRouteBean;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+ 
+public class MixReaperClient {
+	private ClientConnectionManager clientConnectionManager = null;
+	private HttpParams params = null;
+	private HttpContext httpContext = null;
+
+	// Set by user, otherwise "Ghost".
+	private  String clientId = "Ghost";
+	
+	// Set by user in client,otherwise VanitySoft.
+  	 private String webpageUrl = "www.vanity-soft.com";
+ 
+	 private String serverUrl = "http://173.64.86.5:8143";
+ 
+	 private String username = "mixare";
+	 private String password = "04f054fe-ec6a-47d8-b1bf-0354845b4741";
+ 
+	
+	private static MixReaperClient mixReaperClient= null;
+	
+	public static MixReaperClient getInstance(){
+		if (  mixReaperClient == null){
+			 mixReaperClient = new MixReaperClient();
+		}
+		return mixReaperClient;
+	}
+
+	private MixReaperClient(){
+ 
+		URI url = null;
+		try {
+			url = new URI(serverUrl);
+		} catch (URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+		HttpHost reaperFireServer = new HttpHost(url.getHost(), url.getPort());
+		HttpRoute httpRoute = new HttpRoute(reaperFireServer);
+		params = new BasicHttpParams();
+ 
+		ConnManagerParams.setMaxTotalConnections(params, 5);
+		ConnPerRouteBean connPerRoute = new ConnPerRouteBean(20);
+		connPerRoute.setMaxForRoute(httpRoute , 50);
+		ConnManagerParams.setMaxConnectionsPerRoute(params, connPerRoute);
+
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(
+		        new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+ 
+	   clientConnectionManager = new ThreadSafeClientConnManager(params, schemeRegistry);
+		
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(
+		    new AuthScope(url.getHost(),url.getPort()), 
+		    new UsernamePasswordCredentials(username, password));
+
+		httpContext = new BasicHttpContext();
+		httpContext.setAttribute(ClientContext.CREDS_PROVIDER, credsProvider);
+	}
+	
+	public void post(Date time,double bearing,double altitude,double latitude,double longtitude) {
+		Date currentDate = new Date(System.currentTimeMillis());
+		Log.println(Log.DEBUG, DataView.class.getName(), "Current Date ["
+				+ DateFormat.getInstance().format(currentDate) + "]");
+	 
+		Log.println(Log.DEBUG, DataView.class.getName(),
+				"Current Date in Location object ["
+						+ DateFormat.getInstance().format(time) + "]");
+		
+		 
+		JSONObject root = new JSONObject();
+		JSONArray eventListArray  = new JSONArray();
+
+		try {
+			
+			JSONObject eventJson = new JSONObject();		
+			eventJson.put("dateTime",DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.FULL).format(time));
+			eventJson.put("azimuth",  bearing );
+			eventJson.put("uuid",  clientId );
+			eventJson.put("inclination", altitude );
+			eventJson.put("info",  webpageUrl );
+			eventJson.put("type", "Point" );
+		 					
+			JSONArray coordinates = new JSONArray();
+				coordinates.put(latitude);
+				coordinates.put(longtitude);			
+			 eventJson.put("coordinates", coordinates );
+			
+			eventListArray.put( eventJson );
+			
+			root.put("eventList",eventListArray);
+			 
+
+		} catch (JSONException e1) {
+			Log.println(Log.ERROR,MixReaperClient.class.getName(),e1.getMessage());
+		}
+	 
+		HttpClient httpclient = new DefaultHttpClient(clientConnectionManager,
+				params);
+		HttpPost httpPost = new HttpPost(serverUrl  + "/reaperfire/v1/events.json");
+		StringEntity entity = null;
+		try {
+			entity = new StringEntity(root.toString());
+
+			httpPost.setEntity(entity);
+
+			httpPost.setHeader("Accept", "application/json");
+			httpPost.setHeader("Content-type", "application/json");
+
+			HttpResponse response;
+
+			Log.println(Log.DEBUG, DataView.class.getName(),
+					"Posting to Reaper Servers" + root.toString() + "...");
+
+			response = httpclient.execute(httpPost, httpContext);
+			if (response.getStatusLine().getStatusCode() != 201) {
+				Log.println(Log.ERROR, DataView.class.getName(),"did not create" + response
+						.getStatusLine().getReasonPhrase()
+						+ " code:" + response.getStatusLine().getStatusCode());
+			} else {
+				Log.println(Log.DEBUG, DataView.class.getName(), "Posted.");
+			}
+
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			httpclient.getConnectionManager().closeExpiredConnections();
+		}
+
+	}
+	
+	/**
+	 * Returns Mixare result JSON
+	 * 
+	 * @param latitude
+	 * @param longtitude
+	 * @param radiusMiles
+	 * @return
+	 */
+	public String get(double latitude,double longtitude,double radiusMiles){
+		
+		JSONObject jsonOject = null;
+		try {
+			jsonOject = new JSONObject("{\"type\": \"Point\", \"coordinates\": ["+Double.toString(latitude)+","+Double.toString(longtitude)+"],\"radius\":\""+Double.toString(radiusMiles)+ "\"}");
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		//Last 30 days of hits only.
+		 Calendar calendar = Calendar.getInstance();
+		 Date endDate  = (calendar.getTime());
+		 calendar.add(Calendar.DATE, -30);
+		 Date startDate = calendar.getTime();
+
+		List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+		parameters.add(new BasicNameValuePair("startDateTime",DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.FULL).format(startDate)));
+		parameters.add(new BasicNameValuePair("endDateTime",DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.FULL).format(endDate)));
+		parameters.add(new BasicNameValuePair("pageNumber","1"));
+		parameters.add(new BasicNameValuePair("pageSize","10"));
+		parameters.add(new BasicNameValuePair("query",jsonOject.toString()));
+		URLEncodedUtils.format(parameters,"UTF-8");
+		HttpGet httpGet = new HttpGet(serverUrl  + "/reaperfire/v1/search/radius.json?"+URLEncodedUtils.format(parameters,"UTF-8"));
+		String content = null;
+		HttpClient httpclient = null;
+		try {
+			HttpResponse response; 
+			httpclient = new DefaultHttpClient(clientConnectionManager,params);
+		    response = httpclient.execute(httpGet, httpContext);
+		    if ( response.getStatusLine().getStatusCode() != 200 ){
+		    	 Log.println(Log.ERROR,MixContext.class.getName(), response.getStatusLine().getReasonPhrase() + " code:" +response.getStatusLine().getStatusCode() );
+		    	 return statusNotOk();
+		    }
+		    
+		    InputStream inputStream  = response.getEntity().getContent();
+		    Writer writer = new StringWriter();
+		    char[] buffer = new char[1024];
+		    try{
+		    	Reader reader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
+		    	int n ;
+		    	while(( n=reader.read(buffer)) != -1){
+		    		writer.write(buffer,0,n);
+		    	}
+		    }finally{
+		    	inputStream.close();
+		    }
+		    
+		    content  = writer.toString();
+		    Log.println(Log.DEBUG,MixContext.class.getName(), "Received " + content);
+		    
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			  httpclient.getConnectionManager().closeExpiredConnections();
+		}
+		
+		return covertToMix(content);
+	}
+
+	private String statusNotOk() {
+	 return "{\"status\": \"NOT OK\"}";
+		 
+	}
+
+	/**
+	 * received
+	 * 		{
+			executionTime(ms): 263
+			totalHitCount: 1
+			-eventList: [
+							-{
+								id: "04f054fe-ec6a-47d8-b1bf-0354845b4741"
+								uuid: "sample"
+								latitude: 38.9955
+								longtitude: -77.2884
+								dateTime: "Nov 1, 2010 2:14 PM"
+								geoDistance: 0
+								distance: 5.5689674378856955
+								azimuth: 31
+								inclination: 234
+								llm: 5.5689674378856955
+							}
+						]
+			} 
+	 * @param content
+	 * @return
+	 */
+	private String covertToMix(String content) {
+	 
+		String mixJson = null;
+		 try {
+			JSONObject reaperJsonObject = new JSONObject(content);
+			JSONObject mixJsonObject = new JSONObject();
+			mixJsonObject.put("status","OK");
+			mixJsonObject.put("num_results", reaperJsonObject.getInt("totalHitCount"));
+			
+			JSONArray mixJsonResultArray = new JSONArray();
+			JSONArray reaperJsonEventArray = reaperJsonObject.getJSONArray("eventList");
+			for(int index=0;index< reaperJsonEventArray.length();index++){
+				JSONObject event = reaperJsonEventArray.getJSONObject(index);
+				JSONObject mixItem = new JSONObject();
+				mixItem.put("id", event.get("id"));
+				mixItem.put("lat", event.get("latitude"));
+				mixItem.put("lng", event.get("longitude"));
+				mixItem.put("elevation", event.get("azimuth"));
+				
+				BigDecimal bd = new BigDecimal(event.getDouble("distance"));
+				bd = bd.setScale(2,BigDecimal.ROUND_HALF_UP);
+			 
+				mixItem.put("distance", event.getDouble("distance") );
+			 
+				mixItem.put("title", event.get("uuid") + "(" + bd.doubleValue() + "m), "  +  event.get("dateTime") );
+				if ( event.isNull("info") ){
+					mixItem.put("has_detail_page", 0);
+				}else{
+					mixItem.put("has_detail_page", 1);
+					mixItem.put("webpage", event.get("metadata"));
+				}
+				mixJsonResultArray.put(mixItem);
+			}
+			
+			mixJsonObject.put("results", mixJsonResultArray);
+			
+			mixJson  = mixJsonObject.toString();
+			 
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		 
+		Log.println(Log.DEBUG,MixContext.class.getName(), "MixJSON from Reaper" + mixJson);
+		return mixJson;
+	}
+	
+	
+	public String getClientId() {
+		return clientId;
+	}
+	public String getWebpageUrl() {
+		return webpageUrl;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public void setWebpageUrl(String webpageUrl) {
+		this.webpageUrl = webpageUrl;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/org/mixare/data/DataSource.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2010- Peer internet solutions
+ * 
+ * This file is part of mixare.
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
+package org.mixare.data;
+
+import org.mixare.MixListView;
+import org.mixare.R;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+
+/**
+ * @author hannes
+ *
+ */
+public class DataSource {
+	
+	// Datasource and dataformat are not the same. datasource is where the data comes from
+	// and dataformat is how the data is formatted. 
+	// this is necessary for example when you have multiple datasources with the same
+	// dataformat
+	public enum DATASOURCE { WIKIPEDIA, BUZZ, TWITTER, OSM, OWNURL};
+	public enum DATAFORMAT { WIKIPEDIA, BUZZ, TWITTER, OSM, MIXARE};	
+
+	/** default URL */
+	private static final String WIKI_BASE_URL = "http://ws.geonames.org/findNearbyWikipediaJSON";
+	//private static final String WIKI_BASE_URL =	"file:///sdcard/download/data.json";
+	private static final String TWITTER_BASE_URL = "http://search.twitter.com/search.json";
+	private static final String BUZZ_BASE_URL = "https://www.googleapis.com/buzz/v1/activities/search?alt=json&max-results=20";
+	// OpenStreetMap API see http://wiki.openstreetmap.org/wiki/Xapi
+	// eg. only railway stations:
+	//private static final String OSM_BASE_URL =	"http://www.informationfreeway.org/api/0.6/node[railway=station]";
+	//private static final String OSM_BASE_URL =	"http://xapi.openstreetmap.org/api/0.6/node[railway=station]";
+	private static final String OSM_BASE_URL =		"http://osmxapi.hypercube.telascience.org/api/0.6/node[railway=station]";
+	//all objects that have names: 
+	//String OSM_URL = "http://xapi.openstreetmap.org/api/0.6/node[name=*]"; 
+	//caution! produces hugh amount of data (megabytes), only use with very small radii or specific queries
+
+	public static Bitmap twitterIcon;
+	public static Bitmap buzzIcon;
+	
+	public DataSource() {
+		
+	}
+	
+	public static void createIcons(Resources res) {
+		twitterIcon=BitmapFactory.decodeResource(res, R.drawable.twitter);
+		buzzIcon=BitmapFactory.decodeResource(res, R.drawable.buzz);
+	}
+	
+	public static Bitmap getBitmap(DATASOURCE ds) {
+		Bitmap bitmap=null;
+		switch (ds) {
+			case TWITTER: bitmap=twitterIcon; break;
+			case BUZZ: bitmap=buzzIcon; break;
+		}
+		return bitmap;
+	}
+	
+	public static DATAFORMAT dataFormatFromDataSource(DATASOURCE ds) {
+		DATAFORMAT ret;
+		switch (ds) {
+			case WIKIPEDIA: ret=DATAFORMAT.WIKIPEDIA; break;
+			case BUZZ: ret=DATAFORMAT.BUZZ; break;
+			case TWITTER: ret=DATAFORMAT.TWITTER; break;
+			case OSM: ret=DATAFORMAT.OSM; break;
+			case OWNURL: ret=DATAFORMAT.MIXARE; break;
+			default: ret=DATAFORMAT.MIXARE; break;
+		}
+		return ret;
+	}
+	
+	public static String createRequestURL(DATASOURCE source, double lat, double lon, double alt, float radius,String locale) {
+		String ret="";
+		switch(source) {
+		
+			case WIKIPEDIA: 
+				ret= WIKI_BASE_URL + 
+				"?lat=" + lat +
+				"&lng=" + lon + 
+				"&radius="+ radius +
+				"&maxRows=50" +
+				"&lang=" + locale; 
+			break;
+			
+			case BUZZ: 
+				ret= BUZZ_BASE_URL + 
+				"&lat=" + lat+
+				"&lon=" + lon + 
+				"&radius="+ radius*1000;
+			break;
+			
+			case TWITTER: 
+				ret = TWITTER_BASE_URL +
+				"?geocode=" + lat + "%2C" + lon + "%2C" + 
+				Math.max(radius, 1.0) + "km" ;				
+			break;
+				
+			case OSM: 
+				ret = OSM_BASE_URL + XMLHandler.getOSMBoundingBox(lat, lon, radius);
+			break;
+			
+			case OWNURL:
+				ret = MixListView.customizedURL +  
+				"?latitude=" + Double.toString(lat) + 
+				"&longitude=" + Double.toString(lon) + 
+				"&altitude=" + Double.toString(alt) +
+				"&radius=" + Double.toString(radius);
+			break;
+			
+		}
+		return ret;
+	}
+	
+	public static int getColor(DATASOURCE datasource) {
+		int ret;
+		switch(datasource) {
+			case BUZZ:		ret=Color.rgb(4, 228, 20); break;
+			case TWITTER:	ret=Color.rgb(50, 204, 255); break;
+			case OSM:		ret=Color.rgb(255, 168, 0); break;
+			case WIKIPEDIA:	ret=Color.RED; break;
+			default:		ret=Color.WHITE; break;
+		}
+		return ret;
+	}
+
+}
